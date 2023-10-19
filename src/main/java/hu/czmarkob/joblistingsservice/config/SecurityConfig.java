8761face1627480444272ee72a8c457ec94f0894
bczmarko <bczmarko@intrend.hu>
2023-10-19 13:29:30 +0200

    csrf disabled and postgresql dependency added

--- src/main/java/hu/czmarkob/joblistingsservice/config/SecurityConfig.java
+++ src/main/java/hu/czmarkob/joblistingsservice/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,6 +29,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+			.csrf(AbstractHttpConfigurer::disable)
 			.httpBasic(Customizer.withDefaults())
 			.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
 					SessionCreationPolicy.STATELESS))
